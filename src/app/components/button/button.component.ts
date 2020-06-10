5f1f819de7bb7365a67793510182a0b9ff3c3a96
BekkaliyevAssan <marina.neymar.13@gmail.com>
2020-06-10 15:14:14 +0500

    new home page

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
   @Input() text
+  @Input() type
   constructor() { }
 
   ngOnInit(): void {
