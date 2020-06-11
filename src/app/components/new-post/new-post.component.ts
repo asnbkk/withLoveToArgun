22eda0d43cc4f7f084e3eb4175c6d8522f5a28f4
BekkaliyevAssan <marina.neymar.13@gmail.com>
2020-06-11 17:48:54 +0500

    shitty camera

--- src/app/components/new-post/new-post.component.ts
+++ src/app/components/new-post/new-post.component.ts
@@ -1,43 +1,75 @@
 import { Component, OnInit } from '@angular/core';
+import { Page } from "tns-core-modules/ui/page"
 import { takePicture } from 'nativescript-camera'
-import * as imageSourceModule from "tns-core-modules/image-source"
-var fs = require("file-system")
+import { ImageSource, fromFile, fromResource, fromBase64 } from "tns-core-modules/image-source";
+import { Folder, path, knownFolders } from "tns-core-modules/file-system";
+import { ImageAsset } from "tns-core-modules/image-asset";
+let fs = require("file-system")
 import * as Permissions from "nativescript-permissions";
+declare let android: any;
 
-declare var android: any;
 @Component({
   selector: 'ns-new-post',
   templateUrl: './new-post.component.html',
   styleUrls: ['./new-post.component.css']
 })
 export class NewPostComponent implements OnInit {
-  public saveImage
+  public saveImaged
   constructor() { }
 
   ngOnInit(): void {
     Permissions.requestPermission(android.Manifest.permission.CAMERA, "Needed for connectivity status").then(() => {
       console.log("Permission granted!");
-  }).catch(() => {
+    }).catch(() => {
       console.log("Permission is not granted (sadface)");
-  });
+    });
   }
 
-
   openCam() {
-    
-
-
-    var mill = (new Date).getTime()
-    var that = this
+    // const page: Page = <Page>args.object.page;
+    // const vm = page.bindingContext;
+    // const folder: Folder = <Folder>knownFolders.currentApp();
+    // const pathImage = path.join(folder.path, "images/logo.png");
+    // const imageAsset = new ImageAsset(pathImage);
+    // imageAsset.options = {
+    //   width: 100,
+    //   height: 100,
+    //   keepAspectRatio: true
+    // };
+    // let mill = (new Date).getTime()
+    // const source = new ImageSource();
+    // ImageSource.fromAsset(imageAsset)
+    //   .then((imageSource: ImageSource) => {
+    //     const folderPath: string = knownFolders.documents().path;
+    //     const fileName = "test.png";
+    //     const filePath = path.join(folderPath, fileName + mill + ".png")
+    //     const saved: boolean = imageSource.saveToFile(filePath, "png");
+    //     if (saved) {
+    //       console.log("Image saved successfully!");
+    //     }
+    //   })
+    //   .catch((e) => {
+    //     console.log("Error: ");
+    //     console.log(e);
+    //   });
+    let mill = (new Date).getTime()
+    let that = this
     takePicture({width: 300, height: 300, keepAspectRatio: true}).then((function (img) {
-      let source = new imageSourceModule.ImageSource()
-      source.fromAsset(img).then((source) => {
-        let folder = fs.knownFolders.documents()
-        var path = fs.path.join(folder.path,"SaveImage" + mill + ".png")
-        var saved = source.saveToFile(path, "png")
-        this.saveImage = path
-      })
-      
-    }))
+      // const source = new ImageSource()
+      ImageSource.fromAsset(img).then((ImageSource) => {
+        const folder = fs.knownFolders.documents()
+        console.log(fs.knownFolders.documents().path)
+        const path = fs.path.join(folder.path,"image" + mill + ".png")
+        const saved = ImageSource.saveToFile(path, "png")
+        // this.saveImaged = path
+        if (saved) {
+                console.log("Image saved successfully!", path)
+              }
+            })
+            .catch((e) => {
+              console.log("Error: ");
+              console.log(e)
+            });
+     }))
   }
 }
