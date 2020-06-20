7e76f02a19fa04dc02c6805b70a269cd04dff1d3
BekkaliyevAssan <marina.neymar.13@gmail.com>
2020-06-20 17:02:29 +0500

    e;;p

--- src/app/components/new-post/new-post.component.ts
+++ src/app/components/new-post/new-post.component.ts
@@ -4,6 +4,11 @@ import { ImageSource, fromFile, fromResource, fromBase64 } from "tns-core-module
 import { Folder, path, knownFolders } from "tns-core-modules/file-system";
 let imagePicker = require("nativescript-imagepicker")
 import { ImageAsset } from "tns-core-modules/image-asset";
+import * as geolocation from "nativescript-geolocation";
+import { Accuracy } from "tns-core-modules/ui/enums"; 
+import { Mapbox, MapboxViewApi } from "nativescript-mapbox"
+// import { registerElement } from "nativescript-angular/element-registry";
+// registerElement("Mapbox", () => require("nativescript-mapbox").MapboxView);
 // let fs = require("file-system")
 import * as Permissions from "nativescript-permissions";
 declare let android: any;
@@ -20,40 +25,36 @@ export class NewPostComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
+
     Permissions.requestPermission(android.Manifest.permission.CAMERA, "Needed for connectivity status").then(() => {
       console.log("Permission granted!");
     }).catch(() => {
       console.log("Permission is not granted (sadface)");
     });
+  
+    // geolocation.enableLocationRequest()
+    //   .then(() => {
+    //     geolocation.getCurrentLocation({ desiredAccuracy: Accuracy.high })
+    //       .then((location) => {
+    //         alert(JSON.stringify(location))
+    //       })
+    //   })
+    //   .catch(() => {
+        
+    //   })
+  }
+
+  onMapReady(args) {
+    let map: MapboxViewApi = args.map
+    map.getUserLocation().then(
+      function(userLocation) {
+        alert("Current user location" + userLocation)
+      }
+    )
   }
 
   openCam() {
-    // const page: Page = <Page>args.object.page;
-    // const vm = page.bindingContext;
-    // const folder: Folder = <Folder>knownFolders.currentApp();
-    // const pathImage = path.join(folder.path, "images/logo.png");
-    // const imageAsset = new ImageAsset(pathImage);
-    // imageAsset.options = {
-    //   width: 100,
-    //   height: 100,
-    //   keepAspectRatio: true
-    // };
-    // let mill = (new Date).getTime()
-    // const source = new ImageSource();
-    // ImageSource.fromAsset(imageAsset)
-    //   .then((imageSource: ImageSource) => {
-    //     const folderPath: string = knownFolders.documents().path;
-    //     const fileName = "test.png";
-    //     const filePath = path.join(folderPath, fileName + mill + ".png")
-    //     const saved: boolean = imageSource.saveToFile(filePath, "png");
-    //     if (saved) {
-    //       console.log("Image saved successfully!");
-    //     }
-    //   })
-    //   .catch((e) => {
-    //     console.log("Error: ");
-    //     console.log(e);
-    //   });
     let mill = (new Date).getTime()
     let that = this
     takePicture({ width: 300, height: 300, keepAspectRatio: true }).then((function (img) {
